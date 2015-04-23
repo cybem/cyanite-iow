1951201f002a7774919b8aec874af9b92f0dc0d9
Kirill Kondratenko <cybem@cybem.info>
2015-04-23 17:30:30 +0300

    Version 0.1.3-33iow1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.spootnik/cyanite "0.1.3-32iow1"
+(defproject org.spootnik/cyanite "0.1.3-33iow1"
   :description "Alternative storage backend for graphite, backed by cassandra"
   :url "https://github.com/pyr/cyanite"
   :license {:name "MIT License"
